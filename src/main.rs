7c84e37dc9ffbfee1f71c29bac7ea58620173beb
Maxim Kuznetsov <kyzya96@gmail.com>
2023-03-25 18:30:15 +0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -166,7 +166,7 @@ pub async fn webhook(bot: AutoSend<Bot>) -> impl update_listeners::UpdateListene
         .parse()
         .expect("PORT value to be integer");
     // Heroku host example .: "heroku-ping-pong-bot.herokuapp.com"
-    let host = env::var("HOST").expect("have HOST env variable");
+    let host = env::var("HOST_WEBHOOK").expect("have HOST env variable");
     let path = format!("bot{}", teloxide_token);
     let url = Url::parse(&format!("https://{}/{}", host, path)).unwrap();
 
